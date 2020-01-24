500a332908eb0b56c7e2a4c193941d458867f8f1
aneke.deborah@gmail.com <aneke.deborah@gmail.com>
2020-01-24 23:34:24 +0700

    first commit

---
+++ api/queries.js
@@ -0,0 +1,97 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'myapp',
+  password: 'keke1093',
+  port: 5432,
+})
+
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM public.account ORDER BY user_id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.user_id)
+
+  pool.query('SELECT * FROM public.account WHERE user_id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createUser = (request, response) => {
+  const { fullname, email, password } = request.body
+  console.log(request.body)
+
+  pool.query('INSERT INTO public.account (fullname, email, password) VALUES ($1, $2, $3)', [fullname, email, password], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.insertId}`)
+  })
+}
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.user_id)
+  const { fullname, email, user_id } = request.body
+
+  pool.query(
+    'UPDATE public.account SET fullname = $1, email = $2 WHERE user_id = $3',
+    [fullname, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.user_id)
+
+  pool.query('DELETE FROM public.account WHERE user_id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+const login = (request, response) => {
+  const { email, password } = request.body
+
+  pool.query('SELECT email, password FROM users WHERE email = $1', [email], (error, results) => {
+    if (error) {
+      response.status(401).json(error)
+      return
+    }
+    if (result.rows.length === 0) {
+      response.status(404)
+      return
+    }
+    const user = result.rows[0]
+    if (user.password !== password) {
+      response.status(401)
+      return
+    }
+    response.status(200)
+  })
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  login,
+}
